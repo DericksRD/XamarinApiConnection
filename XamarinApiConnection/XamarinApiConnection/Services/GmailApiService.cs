2933c0b68899cd34af0a14ac26e4f3e419c72d46
Derick <derick290801@gmail.com>
2020-12-11 06:13:27 -0400

    Authentication logic completed, but with problems in credentials.json

--- XamarinApiConnection/XamarinApiConnection/Services/GmailApiService.cs
+++ XamarinApiConnection/XamarinApiConnection/Services/GmailApiService.cs
@@ -1,8 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Util.Store;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using XamarinApiConnection.Models;
 
@@ -12,6 +17,7 @@ namespace XamarinApiConnection.Services
     {
         public async Task<GmailUser> GetProfileAsync()
         {
+            Authetincation();
             GmailUser responseUser = null;
             HttpClient client = new HttpClient();
             var response = await client.GetAsync("https://gmail.googleapis.com/gmail/v1/users/{userId}/profile");
@@ -28,6 +34,35 @@ namespace XamarinApiConnection.Services
 
             return responseUser;
         }
+
+        public void Authetincation()
+        {
+            /*
+             * If you want to use any api from google, 
+             * you need to add the authentication Oatuh v.2
+             */
+            string[] Scopes = { GmailService.Scope.GmailReadonly };
+            //string AppliactionName = "Gmail API .Net";
+
+            UserCredential credential;
+
+            using (var stream = new FileStream(@"D:\Documentos\Repos\XamarinApiConnection\XamarinApiConnection\XamarinApiConnection\credentials.json",
+                                    FileMode.Open, FileAccess.Read))
+            {
+                string path = "token.json";
+                //token.json is created automatically and include the user's access.
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(path, true)).Result;
+                /*
+                 * With that code above, we saved the credential file to token.json
+                 */
+            }
+        }
     }
+    
 }
  
